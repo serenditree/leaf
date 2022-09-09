1754697e10da6f00a9b3258227097f81f9275355
Paul Neulinger <paul.neulinger@tanwald.net>
2022-09-09 11:50:15 +0200

    Pylint fix;

--- src/app/about/about.module.ts
+++ src/app/about/about.module.ts
@@ -1,9 +1,9 @@
 import {AboutComponent} from './about/about.component';
 import {CommonModule} from '@angular/common';
+import {ContactComponent} from './contact/contact.component';
 import {MatIconModule} from '@angular/material/icon';
 import {NgModule} from '@angular/core';
 import {TermsComponent} from './terms/terms.component';
-import { ContactComponent } from './contact/contact.component';
 
 @NgModule(
     {
